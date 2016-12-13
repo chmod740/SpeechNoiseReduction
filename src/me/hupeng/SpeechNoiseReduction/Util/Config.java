09d4edbb2fbc491c14b289bd79a1a871a037b71c
hupeng <hupeng@imudges.com>
2016-12-13 16:45:23 +0800

    update

--- src/me/hupeng/SpeechNoiseReduction/Util/Config.java
+++ src/me/hupeng/SpeechNoiseReduction/Util/Config.java
@@ -4,6 +4,6 @@ package me.hupeng.SpeechNoiseReduction.Util;
  * Created by HUPENG on 2016/12/13.
  */
 public class Config {
-    public static String tensorFlowHost = "183.175.12.160";
+    public static String tensorFlowHost = "192.168.1.101";
     public static int port = 9999;
 }
